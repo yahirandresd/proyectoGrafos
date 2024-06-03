e808614ca1fb6c099bae024e049b0297a56415ef
juanchito1234 <162379115+juanchito1234@users.noreply.github.com>
2024-06-03 13:37:58 -0500

    Update calle.py

--- app/model/calle.py
+++ app/model/calle.py
@@ -1,14 +1,30 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from app.model.carro import Carro
+from app.model.centro_operacion import Centro_Operacion
+import random
 
-Calle = nx.Graph()
+class Calle:
+    def __init__(self) -> None:
+        self.calle = nx.Graph()
+        self.centros = []
+        self.crear_calle()
 
-Calle.add_node('Centro de operación A')
-Calle.add_node(2)
-Calle.add_node(3)
-Calle.add_edge('Centro de operación A', 2)
-Calle.add_edge(1, 3)
-Calle.add_edge(2, 3)
+    def crear_calle(self):
+        for i in range(16):
+            self.crear_centro()
 
-nx.draw(Calle, with_labels=True)
-plt.show()
+        self.calle.add_node(self.centros[0].nombre)
+        self.calle.add_node(self.centros[1].nombre)
+        self.calle.add_edge(self.centros[0].nombre, self.centros[1].nombre)
+
+        nx.draw(self.calle, with_labels=True)
+        plt.show()
+
+    def crear_centro(self):
+        centro = Centro_Operacion()
+        centro.set_escoltas(random.randint(10,20))
+        centro.set_vehiculos(random.randint(5,10))
+        centro.set_dinero(random.randint(10000000,100000000)) #cien millones
+
+        self.centros.append(centro)
