d094edafd5f0194fa8dbd87eaf9f987952033009
juanchito1234 <162379115+juanchito1234@users.noreply.github.com>
2024-06-04 11:44:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,17 @@
-from mesa import Agent, Model
-from mesa.time import RandomActivation
-from mesa.space import NetworkGrid
-from mesa.visualization.modules import NetworkModule
-from mesa.visualization.ModularVisualization import ModularServer
+import matplotlib
+matplotlib.use('TkAgg')  # Cambia TkAgg por otro backend si es necesario
+
 from app.model.calle import Calle  # Asegúrate de que esta ruta sea correcta
+import matplotlib.pyplot as plt
 import networkx as nx
 
-class GraphAgent(Agent):
-    def __init__(self, unique_id, model):
-        super().__init__(unique_id, model)
-        self.calle = Calle()
-
-    def step(self):
-        pass
-
-class GraphModel(Model):
+class Main:
     def __init__(self):
-        self.num_agents = 10
-        self.G = nx.erdos_renyi_graph(n=self.num_agents, p=0.2)
-        self.grid = NetworkGrid(self.G)
-        self.schedule = RandomActivation(self)
-        self.calle = Calle()  # Crear instancia de Calle dentro del modelo
+        self.calle = Calle()
+        
+    def run(self):
+        plt.show()  # Muestra el grafo utilizando matplotlib
 
-        for i, node in enumerate(self.G.nodes):
-            agent = GraphAgent(i, self)
-            self.grid.place_agent(agent, node)
-            self.schedule.add(agent)
-
-    def step(self):
-        self.schedule.step()
-
-def network_portrayal(G):
-    portrayal = {}
-    portrayal["nodes"] = [{"id": n} for n in G.nodes]
-    portrayal["edges"] = [{"source": u, "target": v} for u, v in G.edges]
-    return portrayal
-
-network = NetworkModule(network_portrayal, 500, 500)
-
-model_params = {}
-server = ModularServer(GraphModel, [network], "Graph Model", model_params)
-server.port = 8521
-server.launch()
+if __name__ == "__main__":
+    app = Main()
+    app.run()
