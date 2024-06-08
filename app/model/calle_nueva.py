9fd895b33e472aba55e23d1909950940757368ce
juanchito1234 <162379115+juanchito1234@users.noreply.github.com>
2024-06-07 19:58:38 -0500

    Update calle_nueva.py

--- app/model/calle_nueva.py
+++ app/model/calle_nueva.py
@@ -13,6 +13,8 @@ class Calle:
             'Escuela Primaria', 'Universidad Nacional', 'Gimnasio Fitness Plus', 'Clínica Dental Sonrisas']
         self.ubicaciones = []
         self.node_colors = []
+        self.edge_colors = []
+        self.edge_labels = []
         self.crear_calle()
     
     def crear_calle(self):
@@ -29,18 +31,16 @@ class Calle:
         self.crear_centro('Centro E')
         self.crear_cliente('BBVA')
 
-        print(self.ubicaciones)
-
-        self.calle.add_edge(self.ubicaciones[0], self.ubicaciones[18])
+        self.crear_aristas(self.ubicaciones[0], self.ubicaciones[18], 1)
         self.calle.add_edge(self.ubicaciones[0], self.ubicaciones[19])
-        self.calle.add_edge(self.ubicaciones[19], self.ubicaciones[1])
+        self.crear_aristas(self.ubicaciones[19], self.ubicaciones[1], 2)
         self.calle.add_edge(self.ubicaciones[19], self.ubicaciones[6])
         self.calle.add_edge(self.ubicaciones[18], self.ubicaciones[6])
         self.calle.add_edge(self.ubicaciones[18], self.ubicaciones[17])
         self.calle.add_edge(self.ubicaciones[1], self.ubicaciones[16])
         self.calle.add_edge(self.ubicaciones[1], self.ubicaciones[2])
         self.calle.add_edge(self.ubicaciones[16], self.ubicaciones[6])
-        self.calle.add_edge(self.ubicaciones[16], self.ubicaciones[3])
+        self.crear_aristas(self.ubicaciones[16], self.ubicaciones[3], 0)
         self.calle.add_edge(self.ubicaciones[2], self.ubicaciones[3])
         self.calle.add_edge(self.ubicaciones[2], self.ubicaciones[15])
         self.calle.add_edge(self.ubicaciones[15], self.ubicaciones[4])
@@ -48,27 +48,33 @@ class Calle:
         self.calle.add_edge(self.ubicaciones[4], self.ubicaciones[14])
         self.calle.add_edge(self.ubicaciones[14], self.ubicaciones[7])
         self.calle.add_edge(self.ubicaciones[14], self.ubicaciones[5])
-        self.calle.add_edge(self.ubicaciones[5], self.ubicaciones[3])
+        self.crear_aristas(self.ubicaciones[5], self.ubicaciones[3], 1)
         self.calle.add_edge(self.ubicaciones[7], self.ubicaciones[11])
         self.calle.add_edge(self.ubicaciones[11], self.ubicaciones[5])
         self.calle.add_edge(self.ubicaciones[11], self.ubicaciones[12])
-        self.calle.add_edge(self.ubicaciones[12], self.ubicaciones[13])
+        self.crear_aristas(self.ubicaciones[12], self.ubicaciones[13], 1)
         self.calle.add_edge(self.ubicaciones[13], self.ubicaciones[16])
         self.calle.add_edge(self.ubicaciones[13], self.ubicaciones[5])
-        self.calle.add_edge(self.ubicaciones[13], self.ubicaciones[8])
+        self.calle.add_edge(self.ubicaciones[8], self.ubicaciones[13])
         self.calle.add_edge(self.ubicaciones[8], self.ubicaciones[17])
         self.calle.add_edge(self.ubicaciones[8], self.ubicaciones[6])
         self.calle.add_edge(self.ubicaciones[17], self.ubicaciones[10])
         self.calle.add_edge(self.ubicaciones[10], self.ubicaciones[9])
         self.calle.add_edge(self.ubicaciones[9], self.ubicaciones[12])
         self.calle.add_edge(self.ubicaciones[9], self.ubicaciones[8])
-        
 
         self.node_colors = [
             'red' if nodo in [centro.nombre for centro in self.centros] else 
             ('green' if nodo in [cliente.nombre for cliente in self.clientes] else 'blue')
             for nodo in self.calle.nodes
         ]
+        
+        self.edge_colors = [
+            'yellow' if isinstance(self.calle.get_edge_data(edge[0], edge[1]).get('peso'), int) else 'black' 
+            for edge in self.calle.edges
+        ]
+
+        self.edge_labels = nx.get_edge_attributes(self.calle, 'peso')
 
     def crear_edificios(self):
         for item in self.edificios:
@@ -91,8 +97,8 @@ class Calle:
         self.centros.append(centro)
         self.crear_nodo(centro.nombre)
 
-    def crear_aristas(self, edificio1, edificio2):
-        self.calle.add_edge(edificio1, edificio2)
+    def crear_aristas(self, edificio1, edificio2, peso):
+        self.calle.add_edge(edificio1, edificio2, peso = peso)
 
     def crear_nodo(self, edificio):
         self.calle.add_node(edificio)
