1de918a6583046f81eda1abd10be63912d9c2298
juanchito1234 <162379115+juanchito1234@users.noreply.github.com>
2024-06-10 13:21:45 -0500

    Update tipos_carro.py

--- app/model/tipos_carro.py
+++ app/model/tipos_carro.py
@@ -1,7 +1,15 @@
 class Grande:
     def __init__(self, contenedor):
         self.velocidad = 1
-        self.capacidad = contenedor
+        self.peso = contenedor
+
+        if self.peso == 1:
+            self.capacidad = 20000000
+        if self.peso == 2:
+            self.capacidad = 50000000
+        if self.peso == 3:
+            self.capacidad = 100000000
+
         self.escoltas = 2
         self.escudo = 20
         self.ataque = 15
@@ -12,7 +20,15 @@ class Grande:
 class Pequeño:
     def __init__(self, contenedor):
         self.velocidad = 2
-        self.capacidad = contenedor
+        self.peso = contenedor
+
+        if self.peso == 1:
+            self.capacidad = 20000000
+        if self.peso == 2:
+            self.capacidad = 50000000
+        if self.peso == 3:
+            self.capacidad = 100000000
+
         self.escoltas = 1
         self.escudo = 5
         self.ataque = 10
