811231a6fe2f39f0698e777c7e52714d5d5f6b87
juanchito1234 <162379115+juanchito1234@users.noreply.github.com>
2024-06-10 20:12:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,6 @@ class Main:
         self.canvas = FigureCanvasTkAgg(self.figure, master=self.root)
         self.canvas.get_tk_widget().grid(row=2, column=0, columnspan=10, sticky="nsew")
 
-        self.carro = Carro('pequeño')
         self.draw_graph()
 
         image_folder = os.path.join(os.path.dirname(__file__), 'images')
@@ -63,8 +62,6 @@ class Main:
             'grande': mpimg.imread(os.path.join(image_folder, 'cGran.png')),
             'pequeño': mpimg.imread(os.path.join(image_folder, 'cPeque.png')),
         }
-
-        self.update_car_stats()
 
         self.show_center_stats()
 
@@ -103,7 +100,12 @@ class Main:
         self.button.grid(row=4, column=0, columnspan=2, pady=10)
 
     def validar_orden(self):
-        orden = [self.start_node.get(), self.end_node.get(), int(self.amount_spinbox.get()), int(self.tiempo_entry.get())]
+        if int(self.amount_spinbox.get()) < 50:
+            self.carro = Carro('pequeño')
+        else:
+            self.carro = Carro('grande')
+
+        orden = [self.start_node.get(), self.end_node.get(), int(self.amount_spinbox.get()), int(self.tiempo_entry.get()), self.carro]
         tiempo_calculado = self.simular_viaje(orden[0], orden[1])
         orden[3] = tiempo_calculado
 
@@ -112,6 +114,7 @@ class Main:
         if self.validar_capacidad(cantidad, orden):
             if tiempo_calculado < int(self.tiempo_entry.get()):
                 self.ordenes.append(orden)
+                self.show_center_stats()
                 self.show_ordenes()
             else:
                 continuar = tk.Toplevel(self.root)
@@ -172,12 +175,12 @@ class Main:
             stats += f"Nodo fin: {self.ordenes[i][1]}\n"
             stats += f"Cantidad de dinero: {self.ordenes[i][2] * 10**6}\n"
             stats += f"Tiempo estimado: {self.ordenes[i][3]:.2f} segundos\n"
+            stats += f"Tipo de carro: {self.ordenes[i][4].tipo}\n"
             stats += "\n"
 
             self.ordenes_stats.insert(tk.END, stats)
 
         self.ordenes_stats.config(state='disabled')
-
 
     def draw_graph(self):
         self.ax.clear()
@@ -193,22 +196,28 @@ class Main:
             start = item[0]
             end = item[1]
 
+            carro = item[4]
+
+            self.ruta = item[4]
+
+            self.update_car_stats(carro)
+
             inicio_tiempo = time.time()
             if start and end:
                 try:
                     self.path = nx.shortest_path(self.calle.calle, source=start, target=end)
-                    if self.revisar_pesos():
+                    if self.revisar_pesos(carro):
                         self.interpolated_positions = self.interpolate_positions(self.path)
-                        self.carro.get_carro().posicion_actual = self.interpolated_positions[0]
-                        self.animate_path()
+                        carro.posicion_actual = self.interpolated_positions[0]
+                        self.animate_path(carro)
                     else:
                         messagebox.showinfo('Atención', "El puente no puede soportar el peso del vehículo. Buscando ruta alternativa...")
-                        alternative_path = self.find_alternative_route(start, end)
+                        alternative_path = self.find_alternative_route(start, end, carro)
                         if alternative_path:
                             self.path = alternative_path
                             self.interpolated_positions = self.interpolate_positions(self.path)
-                            self.carro.get_carro().posicion_actual = self.interpolated_positions[0]
-                            self.animate_path()
+                            carro.posicion_actual = self.interpolated_positions[0]
+                            self.animate_path(carro)
                         else:
                             messagebox.showinfo('Error', "No se encontró una ruta alternativa.")
                 except nx.NetworkXNoPath:
@@ -231,21 +240,21 @@ class Main:
             continuar.grab_set()
             self.root.wait_window(continuar)
 
-    def revisar_pesos(self):
+    def revisar_pesos(self, carro):
         for i in range(len(self.path) - 1):
             start = self.path[i]
             end = self.path[i + 1]
             edge_data = self.calle.calle.get_edge_data(start, end)
             if edge_data and 'peso' in edge_data:
                 peso_maximo = edge_data['peso']
-                if self.carro.get_carro().peso > peso_maximo:
+                if carro.get_carro().peso > peso_maximo:
                     return False
         return True
     
-    def find_alternative_route(self, start, end):
+    def find_alternative_route(self, start, end, carro):
         G = self.calle.calle.copy()
         
-        edges_to_remove = [(u, v) for u, v, data in G.edges(data=True) if data.get('peso', float('inf')) < self.carro.get_carro().peso]
+        edges_to_remove = [(u, v) for u, v, data in G.edges(data=True) if data.get('peso', float('inf')) < carro.get_carro().peso]
         G.remove_edges_from(edges_to_remove)
         
         try:
@@ -264,8 +273,8 @@ class Main:
                 interpolated_positions.append(interp_pos)
         return interpolated_positions
 
-    def animate_path(self):
-        velocidad = self.carro.get_carro().velocidad
+    def animate_path(self, carro):
+        velocidad = carro.get_carro().velocidad
         frames = len(self.interpolated_positions) // velocidad
         self.anim = FuncAnimation(self.figure, self.update_animation, frames=frames, interval=50, repeat=False)
         self.canvas.draw()
@@ -292,11 +301,11 @@ class Main:
         self.ax.set_ylim(-1, 1)
 
         if i < len(self.interpolated_positions):
-            pos_index = i * self.carro.get_carro().velocidad
+            pos_index = i * self.ruta.get_carro().velocidad
             if pos_index < len(self.interpolated_positions):
-                self.carro.get_carro().actualizar_posicion(self.interpolated_positions[pos_index])
-                current_pos = self.carro.get_carro().posicion_actual
-                carro_tipo = self.carro.tipo
+                self.ruta.get_carro().actualizar_posicion(self.interpolated_positions[pos_index])
+                current_pos = self.ruta.get_carro().posicion_actual
+                carro_tipo = self.ruta.tipo
 
                 if carro_tipo in self.car_images:
                     image = self.car_images[carro_tipo]
@@ -308,13 +317,13 @@ class Main:
                     messagebox.showwarning('Q HUBO GONORREA',"Imagen no encontrada para el tipo de carro:", carro_tipo)
         self.canvas.draw()
 
-    def update_car_stats(self):
-        stats = f"Tipo de carro: {self.carro.tipo}\n"
-        stats += f"Peso: {self.carro.get_carro().peso}\n"
-        stats += f"Velocidad: {self.carro.get_carro().velocidad}\n"
-        stats += f"Escudo: {self.carro.get_carro().escudo}\n"
-        stats += f"Ataque: {self.carro.get_carro().ataque}\n"
-        stats += f"Dinero cargado: {self.carro.get_carro().capacidad}\n"
+    def update_car_stats(self, carro):
+        stats = f"Tipo de carro: {carro.tipo}\n"
+        stats += f"Peso: {carro.get_carro().peso}\n"
+        stats += f"Velocidad: {carro.get_carro().velocidad}\n"
+        stats += f"Escudo: {carro.get_carro().escudo}\n"
+        stats += f"Ataque: {carro.get_carro().ataque}\n"
+        stats += f"Dinero cargado: {carro.get_carro().capacidad}\n"
         
         self.carro_stats.config(state='normal')
         self.carro_stats.delete('1.0', tk.END)
