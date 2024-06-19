a76ac7ab1608d4d8acbfa32c5aa41c53f755cf5a
juanchito1234 <162379115+juanchito1234@users.noreply.github.com>
2024-06-18 21:35:26 -0500

    Update cliente.py

--- app/model/cliente.py
+++ app/model/cliente.py
@@ -1,13 +1,19 @@
 import random
-
+from app.model.contenedor import Contenedor
 
 class Cliente:
     def __init__(self, nombre=None):
         self.nombre = nombre if nombre else self.asignar_nombre()
-        self.dinero_recogido = random.randint(10000000, 100000000)  # entre 10 y 100 millones
+        self.contenedores = [Contenedor(random.randint(1,3)), Contenedor(random.randint(1,3))]
 
     def asignar_nombre(self):
         nombres = [
             'BBVA', 'Éxito', 'Falabella', 'Bancolombia', 'D1'
         ]
         return random.choice(nombres)
+    
+    def eliminar_contenedor(self, contenedor):
+        for i, c in enumerate(self.contenedores):
+            if c.tipo == contenedor:
+                self.contenedores.pop(i)
+                break
