82f5db67cf5e67cbba6a7292c7e08f1911ba30ab
juanchito1234 <162379115+juanchito1234@users.noreply.github.com>
2024-06-18 21:35:14 -0500

    Update contenedor.py

--- app/model/contenedor.py
+++ app/model/contenedor.py
@@ -3,9 +3,12 @@ class Contenedor:
         if tipo == 1:
             self.tipo = 'pequeño'
             self.capacidad = 20
+            self.peso = tipo
         elif tipo == 2:
             self.tipo = 'mediano'
             self.capacidad = 50
+            self.peso = tipo
         elif tipo == 3:
             self.tipo = 'grande'
             self.capacidad = 100
+            self.peso = tipo
